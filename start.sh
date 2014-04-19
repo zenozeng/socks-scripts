c335156ec7d628a1595f84afbcfb9827509b95b1
Zeno Zeng <zenoofzeng@gmail.com>
2014-04-19 19:28:22 +0800

    add 22 to redsocks

--- start.sh
+++ start.sh
@@ -19,3 +19,4 @@ iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 12345
 
 iptables -t nat -A OUTPUT -p tcp --dport 80 -j REDSOCKS
 iptables -t nat -A OUTPUT -p tcp --dport 443 -j REDSOCKS
+iptables -t nat -A OUTPUT -p tcp --dport 22 -j REDSOCKS
--- stop.sh
+++ stop.sh
@@ -1,4 +1,6 @@
 iptables -t nat -D OUTPUT -p tcp --dport 80 -j REDSOCKS
 iptables -t nat -D OUTPUT -p tcp --dport 443 -j REDSOCKS
+iptables -t nat -D OUTPUT -p tcp --dport 22 -j REDSOCKS
+
 iptables -t nat -F REDSOCKS
 iptables -t nat -X REDSOCKS # 删除之前要清空所有引用
